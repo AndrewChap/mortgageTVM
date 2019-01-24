dfd4a34a4f77ce8ebc9c9a45e0498c54cea012a7
Andrew Chap <31779927+AndrewChap@users.noreply.github.com>
2019-01-23 18:41:15 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='mortgagetvm',
+      version='0.1',
+      description='A mortgage calculator that accounts for the Time-Value of Money (TVM)',
+      url='http://github.com/AndrewChap/mortgagetvm',
+      author='Andrew Chap',
+      author_email='andrew@andrewchap.com',
+      license='MIT',
+      packages=['mortgagetvm'])
